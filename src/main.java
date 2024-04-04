87ca34a7296d7db9105c522be0b1a20ed9e8e0a1
CG McDougall <ConnorMcDougall29@outlook.com>
2024-04-04 12:59:55 -0400

    Added setInfo(user,string) to SQLManager: Takes user object and a string table that dictates what table the User info is inserted into. Does not have Unique username checking yet.
    Added updatePersonalInfo() to member:
    Fuction allows change in information using print satements and Scanner.

--- src/main.java
+++ src/main.java
@@ -1,13 +1,14 @@
 package src;
 import java.sql.*;
 
+
 public class main {
 
     public static void main(String[] args) {
 
         SQLManager sql = new SQLManager();
         TestCases t = new TestCases();
-        t.testEquipment();
+        t.TestCase(sql);
 
 
 
